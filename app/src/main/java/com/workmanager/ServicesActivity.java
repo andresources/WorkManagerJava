a4221979cb1b926011896111abbdb55d400afa6f
Harinath <harinathreddy.y1@gmail.com>
2022-07-02 21:35:37 +0530

    This is First

---
+++ app/src/main/java/com/workmanager/ServicesActivity.java
@@ -0,0 +1,22 @@
+package com.workmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ServicesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_services);
+    }
+
+    public void startForegroundService(View view) {
+
+    }
+
+    public void stopForegroundService(View view) {
+    }
+}
